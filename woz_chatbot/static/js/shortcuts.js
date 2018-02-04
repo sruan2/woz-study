0985c396c2678e1588ce0bc3d557e9018f7993da
elzwang <ewang3@stanford.edu>
2018-02-03 19:50:42 -0800

    Added new js file to handle shortcut expansion.

---
+++ woz_chatbot/static/js/shortcuts.js
@@ -0,0 +1,25 @@
+var shortcutPat = new RegExp('/::(\w+)::/', 'g')
+
+var shortcutTable = {
+	name_intro: 'Hi there! Let me briefly introduce myself. \
+				I\'m Ada. Well actually, my real name is Augusta, \
+				but most people just call me Ada.',
+	ada_favs: 'Some of my favorite things to do include spending time with \
+					  family, friends and my dog Buddy - he\'s so fun to play with!',
+	ada_edu: 'For the past few years, I\'ve been studying some of the most \
+				interesting topics in mathematics, science, and languages.',
+	ada_motivate:'I\'m really excited to be sharing some of what I learned with you! \
+				 Let\'s get started!',
+	ada_prompt: 'How about you? Tell me a little about yourself.'
+}
+
+function expandShortcuts(msg) {
+	msg.replace(shortcutPat, function(match, capture){
+		if (shortcutTable.hasOwnProperty(capture))
+			return shortcutTable[capture]
+		else {
+			return match
+		}
+	});
+}
+
