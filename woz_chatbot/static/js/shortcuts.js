53211db155f49f61aa41744feea65dcf8e1ea1a8
“duniahakim” <doniahakim@gmail.com>
2018-02-17 23:52:12 -0800

    added shortcuts

--- woz_chatbot/static/js/shortcuts.js
+++ woz_chatbot/static/js/shortcuts.js
@@ -1,14 +1,15 @@
 var shortcutTable = {
-	name_intro: 'Hi there! Let me briefly introduce myself. \
-				I\'m Ada. Well actually, my real name is Augusta, \
-				but most people just call me Ada.',
-	ada_favs: 'Some of my favorite things to do include spending time with \
-					  family, friends and my dog Buddy - he\'s so fun to play with!',
-	ada_edu: 'For the past few years, I\'ve been studying some of the most \
-				interesting topics in mathematics, science, and languages.',
-	ada_motivate:'I\'m really excited to be sharing some of what I learned with you! \
-				 Let\'s get started!',
-	ada_prompt: 'How about you? Tell me a little about yourself.'
+	intro1: "Hi there! Let me briefly introduce myself. I'm Sally. ",
+	intro2: "I’m studying to be a physicist and astronaut. My dream is to someday go to space!",
+	intro3: "But I also love spending time with family, friends, playing tennis, and writing.",
+	intro4: "For the past few years, I have been studying some of the most interesting topics in mathematics, science, and languages.",
+	intro5: "Today, I'm really excited to be sharing some of what I learned with you! I can't wait to get started!",
+	intro6: "How about you? Tell me a little about yourself.",
+	bye1: "Thanks for going through the mission with me, NAME! I really enjoyed working through it with you.",
+	bye2: "Come back soon!"
+	well: "Well done!",
+	g: "great job!",
+	half: "Don’t forget that the box is only half full!"
 };
 
 function expandShortcuts(msg) {
